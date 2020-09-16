8a1f101c334cb34eb3c02def0e9777b9e9d682d3
Jeremy Thomerson <jeremy@thomersonfamily.com>
2020-09-16 13:44:13 -0400

    feat: expose CLI when package is installed

--- service-web-cli/cli.sh
+++ service-web-cli/cli.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+SCRIPT_DIR="$( cd "$( dirname $( realpath "${BASH_SOURCE[0]}" ) )" && pwd )"
 
 "${SCRIPT_DIR}/../node_modules/.bin/ts-node"  "${SCRIPT_DIR}/src/cli.ts" $@
