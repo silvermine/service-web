silvermine/service-web

commit 5bcf8204d80a01e5df99a4fcd965c9d4dc842e42

feat: add --parallel option for deploys

service-web-cli/src/commands/runForEachDeploymentTarget.ts
@@ -6,12 +6,15 @@ import { ServiceListOptions } from '../../../service-web-core/src/lib/makeServic
 import { StandardOptions } from '../cli';
 import { DeploymentTargetConfig } from '../../../service-web-core/src/config/schemas/auto-generated-types';
 import InputError from '../InputError';
+import { DeploymentGraph } from '../../../service-web-core/src/lib/DeploymentGraph';
 
 interface Progress {
    totalServices: number;
    currentService: number;
    totalTargets: number;
    currentTarget: number;
+   queuedProcesses?: number;
+   unqueuedProcesses?: number;
 }
 
 export interface Runner {
@@ -20,6 +23,7 @@ export interface Runner {
 
 export interface RunForEachDeploymentTargetOptions extends ServiceListOptions, StandardOptions {
    startAtService?: string;
+   parallel?: number;
 }
 
 function filterTargets(opts: StandardOptions, tc: DeploymentTargetConfig): boolean {
@@ -56,53 +60,88 @@ export default async function runForEachDeploymentTarget(web: Web, serviceNames:
       });
    }
 
-   let skipping = !!opts.startAtService,
-       totalServices = 0,
-       currentService = 0,
-       currentTarget = 0,
-       lastService: Service | undefined;
+   if (opts.parallel && opts.parallel > 1) {
+      const deploymentGraph = new DeploymentGraph(web, services, opts.environmentGroup || '');
 
-   const flatChain = makeDeploymentChain(web, opts.environmentGroup || '', services, opts)
-      .reduce((memo, link) => {
-         link.targets.filter(filterTargets.bind(null, opts)).forEach((t) => {
-            memo.push([ link.service, t ]);
+      if (opts.startAtService) {
+         deploymentGraph.pruneDependenciesOf(opts.startAtService).forEach((svcID) => {
+            console.info(`Skipping ${svcID} - will not start until ${opts.startAtService}`);
          });
-         return memo;
-      }, [] as [ Service, DeploymentTargetConfig ][])
-      .filter(([ svc ]) => {
-         if (skipping) {
-            if (svc.ID === opts.startAtService || svc.name === opts.startAtService) {
-               skipping = false;
-               totalServices = totalServices + 1;
+      }
+
+      const servicesToDeploy = deploymentGraph.services(),
+            totalServices = servicesToDeploy.length;
+
+      const totalTargets = servicesToDeploy.reduce((memo, svc) => {
+         return memo + svc.deploymentTargetsFor(opts.environmentGroup || '').filter(filterTargets.bind(null, opts)).length;
+      }, 0);
+
+      let currentService = 0,
+          currentTarget = 0;
+
+      await deploymentGraph.walk(async (svc, targets) => {
+         targets = targets.filter(filterTargets.bind(null, opts));
+
+         currentService = currentService + 1;
+         for (const target of targets) {
+            currentTarget = currentTarget + 1;
+            await runner(svc, target, {
+               currentService,
+               totalServices,
+               currentTarget,
+               totalTargets,
+               queuedProcesses: deploymentGraph.stats().pending,
+               unqueuedProcesses: deploymentGraph.stats().unstarted,
+            });
+         }
+      }, { concurrency: opts.parallel });
+   } else {
+      let skipping = !!opts.startAtService,
+          totalServices = 0,
+          currentService = 0,
+          currentTarget = 0,
+          lastService: Service | undefined;
+
+      const flatChain = makeDeploymentChain(web, opts.environmentGroup || '', services, opts)
+         .reduce((memo, link) => {
+            link.targets.filter(filterTargets.bind(null, opts)).forEach((t) => {
+               memo.push([ link.service, t ]);
+            });
+            return memo;
+         }, [] as [ Service, DeploymentTargetConfig ][])
+         .filter(([ svc ]) => {
+            if (skipping) {
+               if (svc.ID === opts.startAtService || svc.name === opts.startAtService) {
+                  skipping = false;
+                  totalServices = totalServices + 1;
+                  lastService = svc;
+                  return true;
+               }
+               if (lastService !== svc) {
+                  console.info(`Skipping ${svc.ID} - will not start until ${opts.startAtService}`);
+               }
                lastService = svc;
-               return true;
+               return false;
             }
             if (lastService !== svc) {
-               console.info(`Skipping ${svc.ID} - will not start until ${opts.startAtService}`);
+               totalServices = totalServices + 1;
             }
             lastService = svc;
-            return false;
-         }
-         if (lastService !== svc) {
-            totalServices = totalServices + 1;
-         }
-         lastService = svc;
-         return true;
-      });
+            return true;
+         });
 
-   lastService = undefined;
-
-   for (let link of flatChain) {
-      if (lastService !== link[0]) {
-         currentService = currentService + 1;
+      for (let link of flatChain) {
+         if (lastService !== link[0]) {
+            currentService = currentService + 1;
+         }
+         currentTarget = currentTarget + 1;
+         lastService = link[0];
+         await runner(link[0], link[1], {
+            currentService,
+            totalServices,
+            currentTarget,
+            totalTargets: flatChain.length,
+         });
       }
-      currentTarget = currentTarget + 1;
-      lastService = link[0];
-      await runner(link[0], link[1], {
-         currentService,
-         totalServices,
-         currentTarget,
-         totalTargets: flatChain.length,
-      });
    }
 }

service-web-cli/src/cli.ts
@@ -1,4 +1,4 @@
-import { flatten, isEmpty, isUndefined } from '@silvermine/toolbox';
+import { flatten, isEmpty, isNumber, isUndefined } from '@silvermine/toolbox';
 import { loadServiceWeb } from '../../service-web-core/src';
 import { createCommand, CommanderError } from 'commander';
 import { relative } from 'path';
@@ -62,6 +62,7 @@ function addDeploymentTargetBasedCommand(web: Web, cmdName: string, desc: string
       .option('--with-deps', 'Add services that are dependencies of those you\'re running the command on')
       .option('--exact-order', 'Do not do any sorting. Deploy services in the order specified. Only used when service names are specified as arguments.') // eslint-disable-line max-len
       .option('--start-at <service>', 'Start at a given service, skipping all that come before it. Useful for restarting a command that failed part way through.') // eslint-disable-line max-len
+      .option('--parallel <concurrency>', 'Deploy multiple services in parallel when possible.')
       .action(async (cmd) => {
          const opts = Object.assign({}, program.opts(), cmd.opts()),
                standardOpts = getStandardOptions();
@@ -70,11 +71,12 @@ function addDeploymentTargetBasedCommand(web: Web, cmdName: string, desc: string
             addDependencies: Boolean(opts.withDeps),
             reverse: Boolean(opts.reverse),
             skipSort: Boolean(opts.exactOrder),
+            parallel: opts.parallel ? Number(opts.parallel) : undefined,
          };
 
-         if (commandOpts.skipSort && (opts.all || isEmpty(cmd.args) || commandOpts.addDependencies)) {
+         if (commandOpts.skipSort && (opts.all || isEmpty(cmd.args) || commandOpts.addDependencies || commandOpts.parallel)) {
             throw new InputError(
-               '--exact-order only allowed when specifying service names, and not allowed with --with-deps or --all'
+               '--exact-order only allowed when specifying service names, and not allowed with --with-deps, --all, or --parallel'
             );
          }
 
@@ -218,10 +220,28 @@ function addDeploymentTargetBasedCommand(web: Web, cmdName: string, desc: string
 
       addDeploymentTargetBasedCommand(web, cmdName, desc, async (s: Service, t: DeploymentTargetConfig, p) => {
          console.info(`Running ${cmdName} for ${s.ID} in ${t.region}:${t.environmentGroup}:${t.environment}`);
-         console.info(`Progress: Service ${p.currentService} of ${p.totalServices} | Target ${p.currentTarget} of ${p.totalTargets}`);
+
+         const statusParts = [
+            `Progress: Service ${p.currentService} of ${p.totalServices}`,
+            `Target ${p.currentTarget} of ${p.totalTargets}`,
+         ];
+
+         const isRunningInParallel = isNumber(p.queuedProcesses) && isNumber(p.unqueuedProcesses);
+
+         if (isRunningInParallel) {
+            statusParts.push(`Queued: ${p.queuedProcesses}; Unqueued: ${p.unqueuedProcesses}`);
+         }
+
+         console.info(statusParts.join(' | '));
 
          try {
-            await s.runNamedCommand(cmdName, t);
+            const prefix = `${s.system.name}:${s.name}\t${t.environmentGroup}:${t.environment}:${t.region}\t`;
+
+            await s.runNamedCommand(cmdName, t, {
+               // add a prefix to the output when multiple commands are run in parallel to
+               // allow users to regroup the logs if needed
+               outputPrefix: isRunningInParallel ? prefix : undefined,
+            });
          } catch(err) {
             if (err instanceof ShellCommandError) {
                console.error(`Error running ${cmdName} for ${s.ID}: ${err}\n${err.stack}`);

service-web-core/src/model/Service.ts
@@ -59,7 +59,7 @@ export default class Service extends BaseUnit<ServiceConfig> {
          .map((dt) => { return dt.environmentGroup; });
    }
 
-   public async runNamedCommand(cmd: string, target: DeploymentTargetConfig): Promise<void> {
+   public async runNamedCommand(cmd: string, target: DeploymentTargetConfig, opts: { outputPrefix?: string } = {}): Promise<void> {
       return runShellCommands(this.rootDir, this._getCommands(cmd), {
          copyEnv: true,
          env: {
@@ -69,6 +69,7 @@ export default class Service extends BaseUnit<ServiceConfig> {
             SVC_WEB_ENV: target.environment,
             SVC_WEB_REGION: target.region,
          },
+         outputPrefix: opts.outputPrefix,
       });
    }
 

commit 86926d597e28829ee0354ad32592e454da515943

fix: reset lastService iterator
This fixes the regression introduced in 5bcf8204d80a01e5df99a4fcd965c9d4dc842e42 where
serial deploys start at "Progress: Service 0 of 1 | Target 1 of 2"

service-web-cli/src/commands/runForEachDeploymentTarget.ts
@@ -130,6 +130,8 @@ export default async function runForEachDeploymentTarget(web: Web, serviceNames:
             return true;
          });
 
+      lastService = undefined;
+
       for (let link of flatChain) {
          if (lastService !== link[0]) {
             currentService = currentService + 1;